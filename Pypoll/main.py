adb7a99da97a440ba615855aea95dffe3e4c4d70
Nestor Baptista <ndbaptistaf@gmail.com>
2023-05-18 20:22:26 -0400

    Final upload

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,16 +1,12 @@
 import os
 import csv
 
-# Path to the CSV file
+# CSV path
 csv_path = os.path.join('..', 'Python-challenge', 'Pypoll', 'Resources', 'election_data.csv')
 
-# Variables
+# Setting up Variables
 total_votes = 0
-pre_candidates = []
-candidates_unique = []
-count_candidate_0 = 0
-count_candidate_1 = 0
-count_candidate_2 = 0
+candidates_ALL = {}
 
 # Read the CSV file
 with open(csv_path) as csvfile:
@@ -24,30 +20,14 @@ with open(csv_path) as csvfile:
         # Increment the count of total votes
         total_votes += 1
 
-        # Finding the unique candidates in the CSV
-        if candidate_column not in candidates_unique:
-            candidates_unique.append(candidate_column)
-
         # Count the votes for each candidate
-        if candidate_column == candidates_unique[0]:
-            count_candidate_0 += 1
-        elif candidate_column == candidates_unique[1]:
-            count_candidate_1 += 1
-        elif candidate_column == candidates_unique[2]:
-            count_candidate_2 += 1
+        if candidate_column in candidates_ALL:
+            candidates_ALL[candidate_column] += 1
+        else:
+            candidates_ALL[candidate_column] = 1
 
-    # Find the winner based on the counts
-    if count_candidate_0 > count_candidate_1 and count_candidate_0 > count_candidate_2:
-        winner = candidates_unique[0]
-    elif count_candidate_1 > count_candidate_0 and count_candidate_1 > count_candidate_2:
-        winner = candidates_unique[1]
-    elif count_candidate_2 > count_candidate_0 and count_candidate_2 > count_candidate_1:
-        winner = candidates_unique[2]
-
-# Calculate the percentage of votes for each candidate
-percentage_candidate_0 = (count_candidate_0 / total_votes) * 100
-percentage_candidate_1 = (count_candidate_1 / total_votes) * 100
-percentage_candidate_2 = (count_candidate_2 / total_votes) * 100
+# Find the winner based on the vote counts
+winner = max(candidates_ALL, key=candidates_ALL.get)
 
 # Prepare the result text
 text = f"""
@@ -55,9 +35,14 @@ Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------
-{candidates_unique[0]}: {percentage_candidate_0:.3f}% ({count_candidate_0})
-{candidates_unique[1]}: {percentage_candidate_1:.3f}% ({count_candidate_1})
-{candidates_unique[2]}: {percentage_candidate_2:.3f}% ({count_candidate_2})
+"""
+
+#Calculating each candidates avg and add it to text
+for candidate, votes in candidates_ALL.items():
+    percentage = (votes / total_votes) * 100
+    text += f"{candidate}: {percentage:.3f}% ({votes})\n"
+
+text += f"""
 -------------------------
 Winner: {winner}
 -------------------------
@@ -66,7 +51,7 @@ Winner: {winner}
 # Print the result in the console
 print(text)
 
-# Create a text file and write the result
+# Create a text file with the results
 txt_file_name = os.path.join('..', 'Python-challenge', 'Pypoll', 'Election_Results.txt')
 with open(txt_file_name, 'w') as file:
     file.write(text)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Python-challenge
+
+This repository contains two Python scripts, "PyBank" and "Pypoll," that perform data analysis on financial and election data, respectively. These scripts demonstrate the use of fundamental Python programming concepts for data manipulation and analysis.
+
+## PyBank
+
+The "PyBank" script analyzes financial data to derive meaningful insights about a company's profits and losses. The script reads a CSV file containing financial data, calculates various metrics, and generates a summary report. The key features of the script include:
+
+- Reading CSV data: The script uses the `csv` module to read and extract data from a CSV file.
+- Iterations by using "For" and "If" statements: It iterates over each row in the CSV file to extract relevant information, such as dates and profit/loss values.
+- Data aggregation: The script calculates the total number of months, the net profit/loss, and the average change in profit/loss over the given period.
+- Analysis and reporting: It identifies the months with the greatest increase and decrease in profits and generates a comprehensive financial analysis report.
+
+## Pypoll
+
+The "Pypoll" script analyzes election data to determine the election winner and provide insights into voting patterns. The script reads a CSV file containing election data, calculates various metrics, and generates a summary report. The main features of the script include:
+
+- Reading CSV data: The script uses the `csv` module to read and extract data from a CSV file.
+- Using Dictionaries to determine vote counts: It counts the total number of votes and tallies the votes for each candidate using a dictionary data structure.
+- Percentage calculation: It calculates the percentage of votes received by each candidate.
+- Analysis and reporting: The script generates a comprehensive election analysis report.
+
+## Conclusion
+These scripts showcase the power and flexibility of Python for data analysis tasks, utilizing features such as file handling, data manipulation, conditional statements, loops, and data structures.
